d2cada4a2627c49f98c20fa86067b4eeeb69ca9d
GENYST\laura <nunez.laura.e@protonmail.com>
2024-09-23 23:34:39 +0200

    Contact form, responsiv and skill blocks

--- js/main.js
+++ js/main.js
@@ -7,3 +7,31 @@ function hideSidebar() {
     const sidebar = document.querySelector(".sidebar");
     sidebar.style.display = 'none';
 }
+
+function stars() {
+    let count = 600;
+    let scene = document.querySelector(".scene");
+    let i = 0;
+
+    let docHeight = document.documentElement.scrollHeight;
+    
+    while (i < count) {
+        let star = document.createElement("i");
+        let x = Math.floor(Math.random() * window.innerWidth);
+        let y = Math.floor(Math.random() * docHeight);
+        let duration = Math.random() * 10;
+        let size = Math.random() * 2;
+
+        star.style.left = x + 'px';
+        star.style.top = y + 'px';
+        star.style.width = 1 + size + 'px';
+        star.style.height = 1 + size + 'px';
+
+        star.style.animationDuration = 5 + duration + 's';
+        star.style.animationDelay = duration + 's';
+
+        scene.appendChild(star);
+        i++;
+    }
+}
+stars();
